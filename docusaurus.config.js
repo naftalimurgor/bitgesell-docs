5e168cce7f9fd534a7e9fc9e57e95aadf68da3d2
naftalimurgor <murgornaftali@gmail.com>
2023-04-08 17:26:56 +0300

    minor typo fix and wording

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,7 +113,7 @@ const config = {
               },
               {
                 label: 'DAO',
-                href: 'https://manager.daolens.com/app/dao/Bitgesell/1374/overview'',
+                href: 'https://manager.daolens.com/app/dao/Bitgesell/1374/overview',
               },
               {
                 label: 'Discord',
