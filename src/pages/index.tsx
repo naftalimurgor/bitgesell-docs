937e6f91f910ac77fd2d5199ae899430bcdf1987
naftalimurgor <murgornaftali@gmail.com>
2024-08-25 21:50:13 +0300

    revert commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header style={{ background: '#5444C6' }} className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 style={{ color: 'white' }} className="hero__title">{siteConfig.title.toUpperCase()}</h1>
+        <p className="hero__subtitle" style={{ color: 'white' }}>{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/category/wallets">
+            Get started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Intro | ${siteConfig.title}`}
+      description="Bitgesell docs to help developers build o />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
